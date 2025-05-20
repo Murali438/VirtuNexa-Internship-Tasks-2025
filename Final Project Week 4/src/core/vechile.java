59b1eec30bb844927314af99b427040feb669f88
Murali438 <muraliboda740@gmail.com>
2025-05-20 21:14:08 +0530

    Create vechile.java

---
+++ Final Project Week 4/src/core/vechile.java
@@ -0,0 +1,12 @@
+package core;
+
+import java.time.LocalDateTime;
+
+public class Vehicle {
+    private String licensePlate;
+    private String type;
+    private LocalDateTime entryTime;
+    private LocalDateTime exitTime;
+
+    // Constructor, getters, setters
+}
